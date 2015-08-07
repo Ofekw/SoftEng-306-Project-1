ccae7b794e34e1e6d8bfdeb4d78dcbe9c581ffd4
Patrick <pat.stuart1994@gmail.com>
2015-08-07 19:19:01 +1200

    Merge branch 'master' into patrickRobotWorldNav
    Conflicts:
    	robotList.txt
    	se306Project1/src/Entity.py
    	se306Project1/src/Run_Robot0.py
    	world/myworld.world (reverted from commit 1da8c800fb9032e348b633e66718bbeff327b37e)

--- se306Project1/src/Run_Robot1.py
+++ se306Project1/src/Run_Robot1.py
@@ -9,52 +9,17 @@ from tf.transformations import *
 import math
 import numpy.testing
 from Robot import Robot
-import ActionInterruptException
 
 """
 @MAIN
 
-Main function that creates robot and sets a path
+Main function that creates a robot and sets a path
 
 """
 
 def main():
-    #Construction of Robot objects take 3 params... Robot ID, Start X, Start Y. Start X and Start Y correlates to the myworld.world file
-    #Can't create more than one robot per main() .... ie can't run more than one robot per terminal running
-
-    robot0 = Robot(1,0,0)
-
-    rospy.Rate(100)
-    rospy.sleep(0.1)
-
-    print("Current x pos = " + str(robot0.px))
-    print("Current y pos = " + str(robot0.py))
-
-
-    #You can use RobotNode_cmdvel to simulate movements, place them in the while loop to try it out
-    #RobotNode_cmdvel = geometry_msgs.msg.Twist()
-
-    moveAction = robot0._actions_[0], [1000]
-    robot0._actionsStack_.append(moveAction)
-    # turnAction = robot0.actions[2], ["left"]
-    # robot0.actionsStack.append(turnAction)
-
-
-    while not rospy.is_shutdown():
-    #check if there is an action on the stack or an action already running
-        if(robot0._actionsStack_.__len__() > 0 and not robot0._actionRunning_):
-            #get top action on stack
-            action = robot0._actionsStack_[-1]
-            #run action with parameter
-            try:
-                result = action[0](*action[1])
-                robot0._stopCurrentAction_ = False
-                #if action completes succesfully pop it
-                if result == 0:
-                    robot0._actionsStack_.pop()
-            except ActionInterruptException.ActionInterruptException as e:
-                print(e)
-
+    #insert instantiation and path moving here
+    pass
 
 if __name__ == '__main__':
     try:
