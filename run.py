f7c4f9aafa466d15efee2ed938c0ab39c1ad393f
Greggory <greggory_737@live.com>
2015-08-08 14:49:52 +1200

    Completed. run.py script now reads from the config file and generates number of temp .py files to run each picker robot. The temp files are deleted when the program is killed/end.
    Note: All bash commands now run in one terminal

--- run.py
+++ run.py
@@ -4,16 +4,13 @@ import os
 import stat
 import atexit
 
-print subprocess.check_output("rosmake se306Project1", shell=True)
+subprocess.check_output("rosmake se306Project1", shell=True)
 subprocess.Popen("bash -c 'roscore'", shell=True)
 subprocess.Popen("bash -c 'sleep 2 && rosrun stage_ros stageros world/myworld.world'", shell=True)
 
 config = {}
-left_tree = open('world')
-
 with open("config.properties", "r") as f:
     for line in f:
-        print(line)
         property = line.split('=')
         config[property[0]] = property[1]
 number = config.get('robot.number')
@@ -23,18 +20,26 @@ with open("header.txt", "r") as f:
     for line in f:
         string += line
 
+file_name = []
 directory = "./se306Project1/src/"
-name = "r1.py"
-temp = open(os.path.join(directory, name),'a')
-temp.write(string)
-temp.close()
-os.chmod(directory+name,stat.S_IRWXU)
-command = "bash -c 'sleep 3 && rosrun se306Project1 " + name + "'"
-subprocess.Popen(command, shell=True)
+for i in range(0, int(number)):
+    replace = "    robot = RobotPicker(" + str(i) + ", 0, 0, math.pi/2)"
+    name = "r" + str(i) + ".py"
+    file_name.append(name)
+    temp = open(os.path.join(directory, name),'a')
+    temp.write(string.replace("@@@", replace))
+    temp.close()
+    os.chmod(directory+name,stat.S_IRWXU)
+
+for name in file_name:
+    command = "bash -c 'sleep 3 && rosrun se306Project1 " + name + "'"
+    subprocess.Popen(command, shell=True)
 
 def delete_files():
-    os.remove(directory+name)
+    for name in file_name:
+        os.remove(directory+name)
 
 atexit.register(delete_files)
 
 while True:
+ pass
