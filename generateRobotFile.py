9e00a32c6c5ba83e35e17e23b14cf285a9258991
Greggory <greggory_737@live.com>
2015-08-09 12:38:21 +1200

    -Fixed generateRobotFile creating duplicate from appending file. -generateRobotFile now adds robots to myworld file (Had to modify generateWorldFile)

--- generateRobotFile.py
+++ generateRobotFile.py
@@ -4,6 +4,7 @@ import os
 import stat
 import atexit
 
+initial_x = -20
 config = {}
 with open("config.properties", "r") as f:
     for line in f:
@@ -11,10 +12,18 @@ with open("config.properties", "r") as f:
         config[property[0]] = property[1]
 number = config.get('robot.number')
 
-string = ""
-with open("header.txt", "r") as f:
-    for line in f:
-        string += line
+robot = ""
+world = open('world/myworld.world').read()
+for i in range(0, int(number)):
+    robot = robot + "picker( pose [ " + str(initial_x+(i*10))  + " -28 0.000 90 ] name \"r" + str(i) + "\" color \"yellow\")" + "\n"
+robot = robot + "\n"
+myworld = open('world/myworld.world','w')
+world_template = open('world/templates/myworld.template').read()
+myworld.write(world_template)
+myworld.write(robot)
+myworld.close()
+
+string = open('world/templates/robot.template').read()
 
 file_name = []
 directory = "./se306Project1/src/"
@@ -22,7 +31,7 @@ for i in range(0, int(number)):
     replace = "    robot = RobotPicker(" + str(i) + ", 0, 0, math.pi/2)"
     name = "r" + str(i) + ".py"
     file_name.append(name)
-    temp = open(os.path.join(directory, name),'a')
+    temp = open(os.path.join(directory, name),'w')
     temp.write(string.replace("@@@", replace))
     temp.close()
     os.chmod(directory+name,stat.S_IRWXU)
