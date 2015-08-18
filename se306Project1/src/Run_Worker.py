bec8ff4ba25bc3d7a0efee40620745e6543ccc39
JCSanPedro <j.carlosanpedro@gmail.com>
2015-08-18 19:26:50 +1200

    Commiting Run_Worker again

--- se306Project1/src/Run_Worker.py
+++ se306Project1/src/Run_Worker.py
@@ -1,1 +1,33 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import*
+from geometry_msgs.msg import*
+from nav_msgs.msg import*
+from sensor_msgs.msg import*
+from tf.transformations import *
+import math
+import numpy.testing
+from Worker import Worker
+import ActionInterruptException
+
+"""
+@MAIN
+
+Main function that creates robot and sets a path
+
+"""
+
+def main():
+    robot = Worker(2, 10, -20, 0)
+
+    while not rospy.is_shutdown():
+        robot.worker_specific_function()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
 __author__ = 'carvrooom'
