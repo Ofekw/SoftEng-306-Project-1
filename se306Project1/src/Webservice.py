da44900c4b6a7c0704bd32a213b7e17ba71ada30
Harry She <hshe824@aucklanduni.ac.nz>
2015-08-18 17:28:59 +1200

    Corrected JSON generation syntax

--- se306Project1/src/Webservice.py
+++ se306Project1/src/Webservice.py
@@ -14,68 +14,69 @@ Creates JSON strings for each entity
 def create_JSON_strings():
     directory = "./"
     JSON_string = ""
-    JSON_string+="{ Robot_Pickers [ {"
+    JSON_string+="{ \"Robot_Pickers\": [ {"
     for file in os.listdir(directory):
         if file.endswith("pic.sta"):
             f=open(file)
             lines=f.readlines()
-            JSON_string+="\"Name(ID):" +"\""+ lines[0] +"\","
-            JSON_string+="\"Type:" +"\""+ lines[1] +"\","
-            JSON_string+="\"Current_action:" +"\""+ lines[2] +"\","
-            JSON_string+="\"X_Position:" +"\""+ lines[3] +"\","
-            JSON_string+="\"Y_Position:" +"\""+ lines[4] +"\","
-            JSON_string+="\"Theta(Rads):" +"\""+ lines[5] +"\","
-            JSON_string+="\"Current_capacity:" +"\""+ lines[6] +"\"},"
+            JSON_string+="\"Name\":\""+ lines[0].strip() +"\","
+            JSON_string+="\"Type\":\"" +lines[1].strip() +"\","
+            JSON_string+="\"Current_action\":\""+ lines[2].strip() +"\","
+            JSON_string+="\"X_Position\":\""+ lines[3].strip() +"\","
+            JSON_string+="\"Y_Position\":\"" + lines[4].strip() +"\","
+            JSON_string+="\"Theta(Rads)\":\"" + lines[5].strip() +"\","
+            JSON_string+="\"Current_capacity\":\"" + lines[6].strip() +"\"},"
 
     JSON_string = JSON_string[:-1]
-    JSON_string+="]}"
+    JSON_string+="],"
 
-    JSON_string+="{ Robot_Carriers [ {"
+    JSON_string+="\"Robot_Carriers\": [ {"
     for file in os.listdir(directory):
         if file.endswith("car.sta"):
             f=open(file)
             lines=f.readlines()
-            JSON_string+="\"Name(ID):" +"\""+ lines[0] +"\","
-            JSON_string+="\"Type:" +"\""+ lines[1] +"\","
-            JSON_string+="\"Current_action:" +"\""+ lines[2] +"\","
-            JSON_string+="\"X_Position:" +"\""+ lines[3] +"\","
-            JSON_string+="\"Y_Position:" +"\""+ lines[4] +"\","
-            JSON_string+="\"Theta(Rads):" +"\""+ lines[5] +"\","
-            JSON_string+="\"Current_capacity:" +"\""+ lines[6] +"\"},"
+            JSON_string+="\"Name\":\""+ lines[0].strip() +"\","
+            JSON_string+="\"Type\":\"" +lines[1].strip() +"\","
+            JSON_string+="\"Current_action\":\""+ lines[2].strip() +"\","
+            JSON_string+="\"X_Position\":\""+ lines[3].strip() +"\","
+            JSON_string+="\"Y_Position\":\"" + lines[4].strip() +"\","
+            JSON_string+="\"Theta(Rads)\":\"" + lines[5].strip() +"\","
+            JSON_string+="\"Current_capacity\":\"" + lines[6].strip() +"\"},"
 
     JSON_string = JSON_string[:-1]
-    JSON_string+="]}"
+    JSON_string+="],"
 
-    JSON_string+="{ Visitors [ {"
+    JSON_string+=" \"Visitors\": [ {"
     for file in os.listdir(directory):
         if file.endswith("vis.sta"):
             f=open(file)
             lines=f.readlines()
-            JSON_string+="\"Name(ID):" +"\""+ lines[0] +"\","
-            JSON_string+="\"Type:" +"\""+ lines[1] +"\","
-            JSON_string+="\"Current_action:" +"\""+ lines[2] +"\","
-            JSON_string+="\"X_Position:" +"\""+ lines[3] +"\","
-            JSON_string+="\"Y_Position:" +"\""+ lines[4] +"\","
-            JSON_string+="\"Theta(Rads):" +"\""+ lines[5] +"\","
+            JSON_string+="\"Name\":\""+ lines[0].strip() +"\","
+            JSON_string+="\"Type\":\"" +lines[1].strip() +"\","
+            JSON_string+="\"Current_action\":\""+ lines[2].strip() +"\","
+            JSON_string+="\"X_Position\":\""+ lines[3].strip() +"\","
+            JSON_string+="\"Y_Position\":\"" + lines[4].strip() +"\","
+            JSON_string+="\"Theta(Rads)\":\"" + lines[5].strip() +"\"},"
 
     JSON_string = JSON_string[:-1]
-    JSON_string+="]}"
+    JSON_string+="],"
 
-    JSON_string+="{ Animals [ {"
+    JSON_string+=" \"Animals\": [ {"
     for file in os.listdir(directory):
         if file.endswith("ani.sta"):
             f=open(file)
             lines=f.readlines()
-            JSON_string+="\"Name(ID):" +"\""+ lines[0] +"\","
-            JSON_string+="\"Type:" +"\""+ lines[1] +"\","
-            JSON_string+="\"Current_action:" +"\""+ lines[2] +"\","
-            JSON_string+="\"X_Position:" +"\""+ lines[3] +"\","
-            JSON_string+="\"Y_Position:" +"\""+ lines[4] +"\","
-            JSON_string+="\"Theta(Rads):" +"\""+ lines[5] +"\","
+            JSON_string+="\"Name\":\""+ lines[0].strip() +"\","
+            JSON_string+="\"Type\":\"" +lines[1].strip() +"\","
+            JSON_string+="\"Current_action\":\""+ lines[2].strip() +"\","
+            JSON_string+="\"X_Position\":\""+ lines[3].strip() +"\","
+            JSON_string+="\"Y_Position\":\"" + lines[4].strip() +"\","
+            JSON_string+="\"Theta(Rads)\":\"" + lines[5].strip() +"\"},"
 
     JSON_string = JSON_string[:-1]
     JSON_string+="]}"
     print(JSON_string)
+    return JSON_string
 
 #TODO WORKERS!
 
