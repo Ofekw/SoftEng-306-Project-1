a7d5c6ef30a9b8f58f70ca2f7ead32e6c8db44dc
Harry She <hshe824@aucklanduni.ac.nz>
2015-08-18 14:57:22 +1200

    Added String parsing for pickers and carriers

--- se306Project1/src/Webservice.py
+++ se306Project1/src/Webservice.py
@@ -4,7 +4,7 @@ import os
 def create_JSON_strings():
     directory = "./"
     JSON_string = ""
-    JSON_string+="{ Robot Pickers [ {"
+    JSON_string+="{ Robot_Pickers [ {"
     for file in os.listdir(directory):
         if file.endswith("pic.sta"):
             f=open(file)
@@ -17,8 +17,26 @@ def create_JSON_strings():
             JSON_string+="\"Theta(Rads):" +"\""+ lines[5] +"\","
             JSON_string+="\"Current_capacity:" +"\""+ lines[6] +"\"},"
 
-    JSON_string+="\"Current_capacity:" +"\""+ lines[6] +"\"},"
+    JSON_string = JSON_string[:-1]
+    JSON_string+="]}"
+
+    JSON_string+="{ Robot_Carriers [ {"
+    for file in os.listdir(directory):
+        if file.endswith("car.sta"):
+            f=open(file)
+            lines=f.readlines()
+            JSON_string+="\"Name(ID):" +"\""+ lines[0] +"\","
+            JSON_string+="\"Type:" +"\""+ lines[1] +"\","
+            JSON_string+="\"Current_action:" +"\""+ lines[2] +"\","
+            JSON_string+="\"X_Position:" +"\""+ lines[3] +"\","
+            JSON_string+="\"Y_Position:" +"\""+ lines[4] +"\","
+            JSON_string+="\"Theta(Rads):" +"\""+ lines[5] +"\","
+            JSON_string+="\"Current_capacity:" +"\""+ lines[6] +"\"},"
+
+    JSON_string = JSON_string[:-1]
+    JSON_string+="]}"
     print(JSON_string)
+
 
 create_JSON_strings()
 
