c3df3840ada76056f4b56f8afd9d0fd3acc9aac3
Harry She <hshe824@aucklanduni.ac.nz>
2015-08-18 14:49:43 +1200

    Added method to create JSON strings

--- se306Project1/src/Webservice.py
+++ se306Project1/src/Webservice.py
@@ -1,25 +1,43 @@
 __author__ = 'harry'
 import os
 
-def create_JSON_strings:
+def create_JSON_strings():
     directory = "./"
-    i = 0
+    JSON_string = ""
+    JSON_string+="{ Robot Pickers [ {"
     for file in os.listdir(directory):
-        if file.endswith("pic.sta")or file.endswith("car.sta"):
-            with open(file) as f:
-                for line in f:
+        if file.endswith("pic.sta"):
+            f=open(file)
+            lines=f.readlines()
+            JSON_string+="\"Name(ID):" +"\""+ lines[0] +"\","
+            JSON_string+="\"Type:" +"\""+ lines[1] +"\","
+            JSON_string+="\"Current_action:" +"\""+ lines[2] +"\","
+            JSON_string+="\"X_Position:" +"\""+ lines[3] +"\","
+            JSON_string+="\"Y_Position:" +"\""+ lines[4] +"\","
+            JSON_string+="\"Theta(Rads):" +"\""+ lines[5] +"\","
+            JSON_string+="\"Current_capacity:" +"\""+ lines[6] +"\"},"
 
-                    i+=1
-    i=0
-    for file in os.listdir(directory):
-        if file.endswith("vis.sta"):
-            with open(file) as f:
-                for line in f:
-                    i+=1
-    i=0
-    for file in os.listdir(directory):
-        if file.endswith("ani.sta"):
-            with open(file) as f:
-                for line in f:
-                    i+=1
-    i=0
+    JSON_string+="\"Current_capacity:" +"\""+ lines[6] +"\"},"
+    print(JSON_string)
+
+create_JSON_strings()
+
+
+
+    #
+    # for file in os.listdir(directory):
+    #     if file.endswith("pic.sta"):
+    #         with open(file) as f:
+    #             for line in f:
+    #
+    #
+    # for file in os.listdir(directory):
+    #     if file.endswith("vis.sta"):
+    #         with open(file) as f:
+    #             for line in f:
+    #
+    # for file in os.listdir(directory):
+    #     if file.endswith("ani.sta"):
+    #         with open(file) as f:
+    #             for line in f:
+    #                 i+=1
