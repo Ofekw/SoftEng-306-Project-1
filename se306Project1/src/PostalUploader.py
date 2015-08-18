7cc6edf71882c1dfb3b179a177f325baa024e7a9
Harry She <hshe824@aucklanduni.ac.nz>
2015-08-18 20:24:33 +1200

    removed unnecessary files

--- se306Project1/src/PostalUploader.py
+++
@@ -1,7 +0,0 @@
-import ftplib
-
-session = ftplib.FTP('thinkscruffy.com/ros','ros@thinkscruffy.com','network')
-file = open('0pic.sta','rb')                  # file to send
-session.storbinary('STOR 0pic.sta', file)     # send the file
-file.close()                                    # close file and FTP
-session.quit()
