3828a18a52fea75d42d12cd3addd9f096349b526
hsye185 <hsye185@aucklanduni.ac.nz>
2015-08-21 17:32:18 +1200

    Improved test quality, reduced repeated code, general mode readability

--- se306Project1/test/Test_GUI_Updating.py
+++ se306Project1/test/Test_GUI_Updating.py
@@ -12,7 +12,21 @@ import os
 
 class Test_GUI_overlay_read_data(unittest.TestCase,TestModule):
 
+    def createFile(self,file_path,topic_name,entity_type,current_action,px,py,theta,capacity):
 
+        dir = os.path.abspath(file_path)
+        output_file = open(dir, "w")
+        output_file.write(topic_name)
+        output_file.write(entity_type)
+        output_file.write(current_action)
+        output_file.write(px)
+        output_file.write(py)
+        output_file.write(theta)
+        output_file.write(capacity)
+        output_file.close()
+
+    def deleteFile(self,file_path):
+        os.remove(file_path)
 
     def test_gui_picker_read(self):
 
@@ -20,29 +34,31 @@ class Test_GUI_overlay_read_data(unittest.TestCase,TestModule):
 
         gui = GUI_overlay()
         gui.directory = "./se306Project1/src/"
+
         file_path = './se306Project1/src/TESTpic.sta'
-        dir = os.path.abspath(file_path)
-        output_file = open(dir, "w")
-        output_file.write("robot_100\n")
-        output_file.write("Picker\n")
-        output_file.write("Picking fruit\n")
-        output_file.write("0.25\n")
-        output_file.write("0.55\n")
-        output_file.write("3.1415\n")
-        output_file.write("19/20\n")
-        output_file.close()
+        topic_name = "robot_100\n"
+        entity_name = "Picker\n"
+        current_action = "Picking fruit\n"
+        px = "0.25\n"
+        py = "0.55\n"
+        theta = "3.1415\n"
+        capacity = "19/25\n"
+
+        self.createFile(file_path,topic_name,entity_name,current_action,px,py,theta,capacity)
 
         gui.update()
 
-        self.assertEquals(gui.robot_label_list[0].cget("text"), "robot_100\n")
-        self.assertEquals(gui.robot_label_list[1].cget("text"), "Picker\n")
-        self.assertEquals(gui.robot_label_list[2].cget("text"), "Picking fruit\n")
-        self.assertEquals(gui.robot_label_list[3].cget("text"), "0.25\n")
-        self.assertEquals(gui.robot_label_list[4].cget("text"), "0.55\n")
-        self.assertEquals(gui.robot_label_list[5].cget("text"), "3.1415\n")
-        self.assertEquals(gui.robot_label_list[6].cget("text"), "19/20\n")
+        self.deleteFile(file_path)
 
-        os.remove(file_path)
+        self.assertEquals(gui.robot_label_list[0].cget("text"), topic_name)
+        self.assertEquals(gui.robot_label_list[1].cget("text"), entity_name)
+        self.assertEquals(gui.robot_label_list[2].cget("text"), current_action)
+        self.assertEquals(gui.robot_label_list[3].cget("text"), px)
+        self.assertEquals(gui.robot_label_list[4].cget("text"), py)
+        self.assertEquals(gui.robot_label_list[5].cget("text"), theta)
+        self.assertEquals(gui.robot_label_list[6].cget("text"), capacity)
+
+
 
     def test_gui_carrier_read(self):
 
@@ -50,29 +66,29 @@ class Test_GUI_overlay_read_data(unittest.TestCase,TestModule):
 
         gui = GUI_overlay()
         gui.directory = "./se306Project1/src/"
+
         file_path = './se306Project1/src/TESTcar.sta'
-        dir = os.path.abspath(file_path)
-        output_file = open(dir, "w")
-        output_file.write("robot_200\n")
-        output_file.write("Carrier\n")
-        output_file.write("Going to Picker\n")
-        output_file.write("0.25\n")
-        output_file.write("0.55\n")
-        output_file.write("3.1415\n")
-        output_file.write("19/20\n")
-        output_file.close()
+        topic_name = "robot_100\n"
+        entity_name = "Picker\n"
+        current_action = "Picking fruit\n"
+        px = "0.25\n"
+        py = "0.55\n"
+        theta = "3.1415\n"
+        capacity = "19/25\n"
+
+        self.createFile(file_path,topic_name,entity_name,current_action,px,py,theta,capacity)
 
         gui.update()
 
-        self.assertEquals(gui.robot2_label_list[0].cget("text"), "robot_200\n")
-        self.assertEquals(gui.robot2_label_list[1].cget("text"), "Carrier\n")
-        self.assertEquals(gui.robot2_label_list[2].cget("text"), "Going to Picker\n")
-        self.assertEquals(gui.robot2_label_list[3].cget("text"), "0.25\n")
-        self.assertEquals(gui.robot2_label_list[4].cget("text"), "0.55\n")
-        self.assertEquals(gui.robot2_label_list[5].cget("text"), "3.1415\n")
-        self.assertEquals(gui.robot2_label_list[6].cget("text"), "19/20\n")
+        self.deleteFile(file_path)
 
-        os.remove(file_path)
+        self.assertEquals(gui.robot2_label_list[0].cget("text"), topic_name)
+        self.assertEquals(gui.robot2_label_list[1].cget("text"), entity_name)
+        self.assertEquals(gui.robot2_label_list[2].cget("text"), current_action)
+        self.assertEquals(gui.robot2_label_list[3].cget("text"), px)
+        self.assertEquals(gui.robot2_label_list[4].cget("text"), py)
+        self.assertEquals(gui.robot2_label_list[5].cget("text"), theta)
+        self.assertEquals(gui.robot2_label_list[6].cget("text"), capacity)
 
     def test_gui_visitor_read(self):
 
@@ -82,28 +98,28 @@ class Test_GUI_overlay_read_data(unittest.TestCase,TestModule):
         gui.directory = "./se306Project1/src/"
 
         file_path = './se306Project1/src/TESTvis.sta'
-        dir = os.path.abspath(file_path)
-        output_file = open(dir, "w")
-        output_file.write("human_500\n")
-        output_file.write("Visitor\n")
-        output_file.write("Doing random visitor stuff\n")
-        output_file.write("0.25\n")
-        output_file.write("0.55\n")
-        output_file.write("3.1415\n")
-        output_file.write("12/20\n")
-        output_file.close()
+        topic_name = "robot_100\n"
+        entity_name = "Picker\n"
+        current_action = "Picking fruit\n"
+        px = "0.25\n"
+        py = "0.55\n"
+        theta = "3.1415\n"
+        capacity = "19/25\n"
+
+        self.createFile(file_path,topic_name,entity_name,current_action,px,py,theta,capacity)
 
         gui.update()
 
-        self.assertEquals(gui.human_label_list[0].cget("text"), "human_500\n")
-        self.assertEquals(gui.human_label_list[1].cget("text"), "Visitor\n")
-        self.assertEquals(gui.human_label_list[2].cget("text"), "Doing random visitor stuff\n")
-        self.assertEquals(gui.human_label_list[3].cget("text"), "0.25\n")
-        self.assertEquals(gui.human_label_list[4].cget("text"), "0.55\n")
-        self.assertEquals(gui.human_label_list[5].cget("text"), "3.1415\n")
-        self.assertEquals(gui.human_label_list[6].cget("text"), "12/20\n")
+        self.deleteFile(file_path)
 
-        os.remove(file_path)
+        self.assertEquals(gui.human_label_list[0].cget("text"), topic_name)
+        self.assertEquals(gui.human_label_list[1].cget("text"), entity_name)
+        self.assertEquals(gui.human_label_list[2].cget("text"), current_action)
+        self.assertEquals(gui.human_label_list[3].cget("text"), px)
+        self.assertEquals(gui.human_label_list[4].cget("text"), py)
+        self.assertEquals(gui.human_label_list[5].cget("text"), theta)
+        self.assertEquals(gui.human_label_list[6].cget("text"), capacity)
+
 
     def test_gui_worker_read(self):
 
@@ -111,29 +127,29 @@ class Test_GUI_overlay_read_data(unittest.TestCase,TestModule):
 
         gui = GUI_overlay()
         gui.directory = "./se306Project1/src/"
-        file_path = './se306Project1/src/TESTwor.sta'
-        dir = os.path.abspath(file_path)
-        output_file = open(dir, "w")
-        output_file.write("human_500\n")
-        output_file.write("Visitor\n")
-        output_file.write("Doing random visitor stuff\n")
-        output_file.write("0.25\n")
-        output_file.write("0.55\n")
-        output_file.write("3.1415\n")
-        output_file.write("12/20\n")
-        output_file.close()
+
+        file_path = './se306Project1/src/TESTvis.sta'
+        topic_name = "robot_100\n"
+        entity_name = "Picker\n"
+        current_action = "Picking fruit\n"
+        px = "0.25\n"
+        py = "0.55\n"
+        theta = "3.1415\n"
+        capacity = "19/25\n"
+
+        self.createFile(file_path,topic_name,entity_name,current_action,px,py,theta,capacity)
 
         gui.update()
 
-        self.assertEquals(gui.human_label_list[0].cget("text"), "human_500\n")
-        self.assertEquals(gui.human_label_list[1].cget("text"), "Visitor\n")
-        self.assertEquals(gui.human_label_list[2].cget("text"), "Doing random visitor stuff\n")
-        self.assertEquals(gui.human_label_list[3].cget("text"), "0.25\n")
-        self.assertEquals(gui.human_label_list[4].cget("text"), "0.55\n")
-        self.assertEquals(gui.human_label_list[5].cget("text"), "3.1415\n")
-        self.assertEquals(gui.human_label_list[6].cget("text"), "12/20\n")
+        self.deleteFile(file_path)
 
-        os.remove(file_path)
+        self.assertEquals(gui.human_label_list[0].cget("text"), topic_name)
+        self.assertEquals(gui.human_label_list[1].cget("text"), entity_name)
+        self.assertEquals(gui.human_label_list[2].cget("text"), current_action)
+        self.assertEquals(gui.human_label_list[3].cget("text"), px)
+        self.assertEquals(gui.human_label_list[4].cget("text"), py)
+        self.assertEquals(gui.human_label_list[5].cget("text"), theta)
+        self.assertEquals(gui.human_label_list[6].cget("text"), capacity)
 
     def test_gui_animal_read(self):
 
@@ -143,28 +159,28 @@ class Test_GUI_overlay_read_data(unittest.TestCase,TestModule):
         gui.directory = "./se306Project1/src/"
 
         file_path = './se306Project1/src/TESTani.sta'
-        dir = os.path.abspath(file_path)
-        output_file = open(dir, "w")
-        output_file.write("human_500\n")
-        output_file.write("Visitor\n")
-        output_file.write("Doing random visitor stuff\n")
-        output_file.write("0.25\n")
-        output_file.write("0.55\n")
-        output_file.write("3.1415\n")
-        output_file.write("12/20\n")
-        output_file.close()
+        topic_name = "robot_100\n"
+        entity_name = "Picker\n"
+        current_action = "Picking fruit\n"
+        px = "0.25\n"
+        py = "0.55\n"
+        theta = "3.1415\n"
+        capacity = "19/25\n"
+
+        self.createFile(file_path,topic_name,entity_name,current_action,px,py,theta,capacity)
 
         gui.update()
 
-        self.assertEquals(gui.animal_label_list[0].cget("text"), "human_500\n")
-        self.assertEquals(gui.animal_label_list[1].cget("text"), "Visitor\n")
-        self.assertEquals(gui.animal_label_list[2].cget("text"), "Doing random visitor stuff\n")
-        self.assertEquals(gui.animal_label_list[3].cget("text"), "0.25\n")
-        self.assertEquals(gui.animal_label_list[4].cget("text"), "0.55\n")
-        self.assertEquals(gui.animal_label_list[5].cget("text"), "3.1415\n")
-        self.assertEquals(gui.animal_label_list[6].cget("text"), "12/20\n")
+        self.deleteFile(file_path)
 
-        os.remove(file_path)
+        self.assertEquals(gui.animal_label_list[0].cget("text"), topic_name)
+        self.assertEquals(gui.animal_label_list[1].cget("text"), entity_name)
+        self.assertEquals(gui.animal_label_list[2].cget("text"), current_action)
+        self.assertEquals(gui.animal_label_list[3].cget("text"), px)
+        self.assertEquals(gui.animal_label_list[4].cget("text"), py)
+        self.assertEquals(gui.animal_label_list[5].cget("text"), theta)
+        self.assertEquals(gui.animal_label_list[6].cget("text"), capacity)
+
 
 if __name__ == '__main__':
     unittest.main()
