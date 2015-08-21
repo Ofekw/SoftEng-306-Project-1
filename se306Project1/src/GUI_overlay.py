2db0b898b82107956e0f9353e1140fdb11cfd2af
hshe824 <hshe824@aucklanduni.ac.nz>
2015-08-22 11:04:05 +1200

    Updated to include que code

--- se306Project1/src/GUI_overlay.py
+++ se306Project1/src/GUI_overlay.py
@@ -63,12 +63,12 @@ class GUI_overlay(Tkinter.Tk):
 
         nb = ttk.Notebook(self)
         nb.pack(fill='both', expand='yes')
+        status_tab = Frame(nb)
         robot_tab = Frame(nb)
         robot_tab2 = Frame(nb)
         human_tab = Frame(nb)
         animal_tab = Frame(nb)
         self.laser_tab = Frame(nb)
-        status_tab = Frame(nb)
 
         # create the tabs
         nb.add(status_tab,text='Status Overview')
@@ -78,9 +78,13 @@ class GUI_overlay(Tkinter.Tk):
         nb.add(animal_tab, text='Animals')
         nb.add(self.laser_tab, text='Laser-Rangers')
 
+
+        #BEGIN STATUS
+        self.status_label_list = self.setup_status(nb, status_tab)
+
+
         #BEGIN ROBOTS
 
-        self.status_label_list = self.setup_status(nb, status_tab)
 
         self.robot_label_list = self.setup_pickers(nb,robot_tab)
         self.robot2_label_list = self.setup_carriers(nb,robot_tab2)
@@ -1759,11 +1763,11 @@ class GUI_overlay(Tkinter.Tk):
 
         status_tab.Label1 = Label(TLabelframe1)
         status_tab.Label1.place(relx=0.025, rely=0.15, height=19, width=150)
-        status_tab.Label1.configure(text='''Picker Queue:''')
+        status_tab.Label1.configure(text='''Picker Queue (IDs):''')
 
         status_tab.Label2 = Label(TLabelframe1)
         status_tab.Label2.place(relx=0.025, rely=0.3, height=19, width=150)
-        status_tab.Label2.configure(text='''Targeted Pickers:''')
+        status_tab.Label2.configure(text='''Targeted Pickers (IDs):''')
 
         status_tab.Label3 = Label(TLabelframe1)
         status_tab.Label3.place(relx=0.025, rely=0.45, height=19, width=150)
@@ -1786,16 +1790,20 @@ class GUI_overlay(Tkinter.Tk):
         status_tab.TLabel2.configure(relief=FLAT)
 
         status_tab.TLabel3 = ttk.Label(TLabelframe1)
-        status_tab.TLabel3.place(relx=0.4, rely=0.6, height=17, width=150)
+        status_tab.TLabel3.place(relx=0.4, rely=0.45, height=17, width=150)
         status_tab.TLabel3.configure(background=self._bgcolor)
         status_tab.TLabel3.configure(foreground="#0066FF")
         status_tab.TLabel3.configure(relief=FLAT)
 
         status_tab.TLabel4 = ttk.Label(TLabelframe1)
-        status_tab.TLabel4.place(relx=0.4, rely=0.07, height=17, width=150)
+        status_tab.TLabel4.place(relx=0.4, rely=0.6, height=17, width=150)
         status_tab.TLabel4.configure(background=self._bgcolor)
         status_tab.TLabel4.configure(foreground="#006600")
         status_tab.TLabel4.configure(relief=FLAT)
+
+        status_label_list = [status_tab.TLabel1,status_tab.TLabel2,status_tab.TLabel3,status_tab.TLabel4]
+
+        return status_label_list
 
 
     #Setup those laser rangers
@@ -2004,6 +2012,13 @@ class GUI_overlay(Tkinter.Tk):
 
     """
     def update(self):
+        i=0
+        for file in os.listdir(self.directory):
+            if file.endswith(".que"):
+                with open(self.directory+file) as f:
+                    for line in f:
+                        self.status_label_list[i].configure(text=str(line))
+                        i+=1
         i = 0
         for file in os.listdir(self.directory):
             if file.endswith("pic.sta"):
