57e37189db57fa652e2dfbb68d3a97ee0f1f6f2b
hsye185 <hsye185@aucklanduni.ac.nz>
2015-08-21 13:41:18 +1200

    Added Carrier Update Test

--- se306Project1/test/Test_GUI_Updating.py
+++ se306Project1/test/Test_GUI_Updating.py
@@ -14,7 +14,7 @@ class Test_GUI_overlay_read_data(unittest.TestCase,TestModule):
 
 
 
-    def test_gui_read(self):
+    def test_gui_picker_read(self):
 
         print self.print_function_name(inspect.stack()[0][3])
 
@@ -42,6 +42,33 @@ class Test_GUI_overlay_read_data(unittest.TestCase,TestModule):
         self.assertEquals(gui.robot_label_list[5].cget("text"), "3.1415\n")
         self.assertEquals(gui.robot_label_list[6].cget("text"), "19/20\n")
 
+    def test_gui_carrier_read(self):
+
+        print self.print_function_name(inspect.stack()[0][3])
+
+        gui = GUI_overlay()
+        gui.directory = "./se306Project1/src/"
+
+        dir = os.path.abspath('./se306Project1/src/TESTcar.sta')
+        output_file = open(dir, "w")
+        output_file.write("robot_200\n")
+        output_file.write("Carrier\n")
+        output_file.write("Going to Picker\n")
+        output_file.write("0.25\n")
+        output_file.write("0.55\n")
+        output_file.write("3.1415\n")
+        output_file.write("19/20\n")
+        output_file.close()
+
+        gui.update()
+
+        self.assertEquals(gui.robot2_label_list[0].cget("text"), "robot_200\n")
+        self.assertEquals(gui.robot2_label_list[1].cget("text"), "Carrier\n")
+        self.assertEquals(gui.robot2_label_list[2].cget("text"), "Going to Picker\n")
+        self.assertEquals(gui.robot2_label_list[3].cget("text"), "0.25\n")
+        self.assertEquals(gui.robot2_label_list[4].cget("text"), "0.55\n")
+        self.assertEquals(gui.robot2_label_list[5].cget("text"), "3.1415\n")
+        self.assertEquals(gui.robot2_label_list[6].cget("text"), "19/20\n")
 
 if __name__ == '__main__':
     unittest.main()
