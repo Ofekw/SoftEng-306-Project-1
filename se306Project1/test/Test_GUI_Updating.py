c409c34d425ea7d732528253b546df0c9a67074e
hsye185 <hsye185@aucklanduni.ac.nz>
2015-08-21 14:13:30 +1200

    Updated model of GUI read tests, now deletes file afterwards. Also added Visitor read update tests

--- se306Project1/test/Test_GUI_Updating.py
+++ se306Project1/test/Test_GUI_Updating.py
@@ -20,8 +20,8 @@ class Test_GUI_overlay_read_data(unittest.TestCase,TestModule):
 
         gui = GUI_overlay()
         gui.directory = "./se306Project1/src/"
-
-        dir = os.path.abspath('./se306Project1/src/TESTpic.sta')
+        file_path = './se306Project1/src/TESTpic.sta'
+        dir = os.path.abspath(file_path)
         output_file = open(dir, "w")
         output_file.write("robot_100\n")
         output_file.write("Picker\n")
@@ -42,14 +42,16 @@ class Test_GUI_overlay_read_data(unittest.TestCase,TestModule):
         self.assertEquals(gui.robot_label_list[5].cget("text"), "3.1415\n")
         self.assertEquals(gui.robot_label_list[6].cget("text"), "19/20\n")
 
+        os.remove(file_path)
+
     def test_gui_carrier_read(self):
 
         print self.print_function_name(inspect.stack()[0][3])
 
         gui = GUI_overlay()
         gui.directory = "./se306Project1/src/"
-
-        dir = os.path.abspath('./se306Project1/src/TESTcar.sta')
+        file_path = './se306Project1/src/TESTcar.sta'
+        dir = os.path.abspath(file_path)
         output_file = open(dir, "w")
         output_file.write("robot_200\n")
         output_file.write("Carrier\n")
@@ -69,6 +71,39 @@ class Test_GUI_overlay_read_data(unittest.TestCase,TestModule):
         self.assertEquals(gui.robot2_label_list[4].cget("text"), "0.55\n")
         self.assertEquals(gui.robot2_label_list[5].cget("text"), "3.1415\n")
         self.assertEquals(gui.robot2_label_list[6].cget("text"), "19/20\n")
+
+        os.remove(file_path)
+
+    def test_gui_visitor_read(self):
+
+        print self.print_function_name(inspect.stack()[0][3])
+
+        gui = GUI_overlay()
+        gui.directory = "./se306Project1/src/"
+
+        file_path = './se306Project1/src/TESTvis.sta'
+        dir = os.path.abspath(file_path)
+        output_file = open(dir, "w")
+        output_file.write("human_500\n")
+        output_file.write("Visitor\n")
+        output_file.write("Doing random visitor stuff\n")
+        output_file.write("0.25\n")
+        output_file.write("0.55\n")
+        output_file.write("3.1415\n")
+        output_file.write("12/20\n")
+        output_file.close()
+
+        gui.update()
+
+        self.assertEquals(gui.human_label_list[0].cget("text"), "human_500\n")
+        self.assertEquals(gui.human_label_list[1].cget("text"), "Visitor\n")
+        self.assertEquals(gui.human_label_list[2].cget("text"), "Doing random visitor stuff\n")
+        self.assertEquals(gui.human_label_list[3].cget("text"), "0.25\n")
+        self.assertEquals(gui.human_label_list[4].cget("text"), "0.55\n")
+        self.assertEquals(gui.human_label_list[5].cget("text"), "3.1415\n")
+        self.assertEquals(gui.human_label_list[6].cget("text"), "12/20\n")
+
+        os.remove(file_path)
 
 if __name__ == '__main__':
     unittest.main()
