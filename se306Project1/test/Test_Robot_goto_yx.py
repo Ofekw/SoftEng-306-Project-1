2708d614ddec9262a4b6780d39e7d01a2870a1ed
hsye185 <hsye185@aucklanduni.ac.nz>
2015-08-22 14:32:17 +1200

    Added YX goto for up and down

--- se306Project1/test/Test_Robot_goto_yx.py
+++ se306Project1/test/Test_Robot_goto_yx.py
@@ -25,7 +25,7 @@ class Test_Robot_goto_yx(unittest.TestCase,TestModule):
     #since we can't reset the stage, we have to work with the same robot.
     robot0 = RobotPicker(0,-20,-28, math.pi/2)
 
-    def test_goto_yx_move_right(self):
+    def test_goto_yx_1move_right(self):
 
         self.print_function_name(inspect.stack()[0][3])
 
@@ -39,7 +39,7 @@ class Test_Robot_goto_yx(unittest.TestCase,TestModule):
         self.assertTrue(self.compare_values_with_threshold(self.robot0.px,end_x),"End X = Expected End X")
         self.assertTrue(self.compare_values_with_threshold(self.robot0.py,end_y),"End Y = Expected End Y")
 
-    def test_goto_yx_move_left(self):
+    def test_goto_yx_2move_left(self):
 
         self.print_function_name(inspect.stack()[0][3])
 
@@ -47,6 +47,34 @@ class Test_Robot_goto_yx(unittest.TestCase,TestModule):
         end_y = -28
 
         moveAction = self.robot0.goto_yx, [end_x,end_y]
+
+        self.run_robot(self.robot0,moveAction,15)
+
+        self.assertTrue(self.compare_values_with_threshold(self.robot0.px,end_x),"End X = Expected End X")
+        self.assertTrue(self.compare_values_with_threshold(self.robot0.py,end_y),"End Y = Expected End Y")
+
+    def test_goto_yx_3move_up(self):
+
+        self.print_function_name(inspect.stack()[0][3])
+
+        end_x = -30
+        end_y = -20
+
+        moveAction = self.robot0.goto_xy, [end_x,end_y]
+
+        self.run_robot(self.robot0,moveAction,15)
+
+        self.assertTrue(self.compare_values_with_threshold(self.robot0.px,end_x),"End X = Expected End X")
+        self.assertTrue(self.compare_values_with_threshold(self.robot0.py,end_y),"End Y = Expected End Y")
+
+    def test_goto_yx_4move_down(self):
+
+        self.print_function_name(inspect.stack()[0][3])
+
+        end_x = -30
+        end_y = -30
+
+        moveAction = self.robot0.goto_xy, [end_x,end_y]
 
         self.run_robot(self.robot0,moveAction,15)
 
